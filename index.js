7bd705fa3c769e69139f9a483eccf2cacdbc8856
kimson14569 <anhyeucay1987@gmail.com>
2019-06-14 21:02:51 +0700

    build join and leave

--- index.js
+++ index.js
@@ -12,8 +12,21 @@ io.on('connection', (socket) => {
     console.log('Connectd')
     socket.on('s-message', (value) => {
         console.log(value)
-        io.sockets.emit('receive-message', value)
+        io.in(room).emit('receive-message', value)
     })
+
+    socket.on('join', (value) => {
+        socket.join(room)
+        io.in(room).emit('joined', value)
+        console.log(`${value.userName} joined`)
+    })
+
+    socket.on('leave', (value) => {
+        socket.leave(room)
+        io.in(room).emit('leaved', value)
+        console.log(`${value.userName} leaved`)
+    })
+
     socket.on('disconnect', () => {
         console.log('Client Disconnected.')
     })
--- nsx/src/App.js
+++ nsx/src/App.js
@@ -1,34 +1,91 @@
 import React from 'react';
 import './App.scss';
 import socketIOClient from 'socket.io-client'
+const socket = socketIOClient('192.168.1.152:5000')
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
       socketServer: '192.168.1.152:5000',
-      receiveMessages: ''
+      receiveMessages: '',
+      buttonTittle: 'Join',
+      userName: 'Kson'
     }
   }
 
   componentDidMount() {
-    let socket = socketIOClient(this.state.socketServer)
     let self = this
     socket.on('receive-message', (value) => {
-      let messages = self.state.receiveMessages
-      messages = messages + '\n' +value
+      this.setMessage(`${value.userName}: ${value.message}`)
+    })
+    this.onJoined()
+    this.onLeaved()
+  }
+
+  onReceived() {
+    socket.on('receive-message', (value) => {
+      this.setMessage(`${value.userName}: ${value.userName}`)
+    })
+  }
+
+  onJoined() {
+    socket.on('joined', (user) => {
+      console.log(user)
+      this.setMessage(`User ${user.userName} joined`)
+    })
+  }
+
+  onLeaved() {
+    socket.on('leaved', (user) => {
+      console.log(user)
+      this.setMessage(`User ${user.userName} leaved`)
+    })
+  }
+
+  setMessage(message) {
+    let self = this
+    let messages = self.state.receiveMessages
+      messages = messages + '\n' + message
       self.setState({
         receiveMessages: messages
       })
-  })
   }
 
   onKeyPress = event => {
-    let socket = socketIOClient(this.state.socketServer)
     if (event.key === 'Enter') {
       console.log(event.target.value)
-      socket.emit('send-message', event.target.value)
+      socket.emit('send-message', {
+        userName: this.state.userName,
+        message: event.target.value
+      })
     }
+  }
+
+  onClick = event => {
+    console.log(event)
+    let title = 'Join'
+    if(this.state.buttonTittle === 'Join') {
+      title = "Leave"
+      this.join()
+    } else {
+      this.leave()
+    }
+    this.setState({
+      buttonTittle: title
+    })
+  }
+
+  join() {
+    socket.emit('join', {
+      userName: this.state.userName
+    })
+  }
+
+  leave() {
+    socket.emit('leave', {
+      userName: this.state.userName
+    })
   }
 
   render() {
@@ -41,6 +98,7 @@ class App extends React.Component {
             </div>
             <div className="send-message">
               <input onKeyPress={this.onKeyPress}></input>
+              <button onClick={e => this.onClick(e)}>{this.state.buttonTittle}</button>
             </div>
           </div>
         </div>
