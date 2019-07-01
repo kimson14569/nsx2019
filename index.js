7afa69e8e10e60bc21acd8e98d611c2d5b7b6417
kimson14569 <anhyeucay1987@gmail.com>
2019-07-01 20:59:08 +0700

    tao database

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const socketIO = require('socket.io')
 const server = http.createServer(app)
 const io = socketIO(server)
 const moment = require('moment')
+const pg = require('pg')
+
+const env = require('dotenv').config()
+
+const config = {
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
+}
+
+const pool = new pg.Pool(config)
 
 const room = 'main'
 io.on('connection', (socket) => {
@@ -19,6 +31,7 @@ io.on('connection', (socket) => {
         value.avatar = createAvatar(value.userName)
         value.create_at = moment().format('MMMM Do YYYY, h:mm:ss a')
         io.in(room).emit('receive-message', value)
+        save2DB(value, room)
     })
     
     socket.on('join', (value) => {
@@ -78,4 +91,13 @@ function convert2HTML(message) {
         .replace(/\:\(/g, '<i class="em em-disappointed"></i>')
         .replace(/\:d/g, '<i class="em em-smiley"></i>')
         .replace(/\:\+1/g, '<i class="em em-ok_hand"></i>')
+}
+
+function save2DB(value, room) {
+    pool.connect(function(err, client, done) {
+        let sql = `insert into room(sent_by, created_at, message) values ('${value.userName}', '${value.create_at}', '${value.message}')`
+        client.query(sql, function(err, result) {
+            done()
+        })
+    })
 }
